85b5befe617cb1d764f262a4afaa09407bfbc7f9
@Draichi <lucasdraichi@gmail.com>
2021-11-02 22:08:28 -0300

    feat: implements apollo client

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -19,4 +19,34 @@ const createApolloClient = () => {
       },
     };
   });
+  const httpLink = new HttpLink({
+    uri: "http://localhost:8000/graphql",
+    credentials: "include",
+  });
+
+  return new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+};
+
+export const initializeApollo = (initialState: any = null) => {
+  const _apolloClient = apolloClient ?? createApolloClient();
+
+  if (initialState) {
+    _apolloClient.cache.restore(initialState);
+  }
+
+  // for SSR
+  if (typeof window === "undefined") return _apolloClient;
+
+  if (!apolloClient) apolloClient = _apolloClient;
+
+  return _apolloClient;
+};
+
+export const useApollo = (initialState: any) => {
+  const store = useMemo(() => initializeApollo(initialState), [initialState]);
+
+  return store;
 };
